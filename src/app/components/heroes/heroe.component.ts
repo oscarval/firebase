f2e0801966f2ca4a3973de07e0b5f60beaf08714
Alexis <entulinea.alexis@gmail.com>
2019-05-27 18:22:12 +0200

    Create services to do get, put and post by firebase. Use the HeroeServices.

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,24 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Heroe } from '../../interfaces/heroe.interface';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Heroe } from "../../interfaces/heroe.interface";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
   styles: []
 })
 export class HeroeComponent implements OnInit {
-  heroe: Heroe = {
-    nombre: '',
-    bio: '',
-    casa: 'Marvel'
+  private heroe: Heroe = {
+    nombre: "",
+    bio: "",
+    casa: "Marvel"
   };
 
-  constructor() {}
+  nuevo: boolean = true;
+  id: string;
+
+  constructor(
+    private router: Router,
+    private heroeServices: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe(parametros => {
+      this.id = parametros["id"];
+
+      if ( this.id !== "nuevo" ){
+        this.heroeServices.getHeroe(this.id).subscribe(
+          data => {
+            this.heroe = data;
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+
+    });
+  }
 
   ngOnInit() {}
 
   guardar() {
-    console.log(this.heroe);
+    if (this.id === "nuevo") {
+      // insertar
+      this.heroeServices.nuevoHeroe(this.heroe).subscribe(
+        data => {
+          this.router.navigate(["/heroe", data.name]);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    } else {
+      // actualizar
+      this.heroeServices.actualizarHeroe(this.heroe, this.id).subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,25 +1,57 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers } from "@angular/http";
 import { Heroe } from "../interfaces/heroe.interface";
-import 'rxjs';
+import "rxjs";
 
 @Injectable()
 export class HeroesService {
   heroesURL: string = "https://heroesapp-6522e.firebaseio.com/heroes.json";
+  heroeURL: string = "https://heroesapp-6522e.firebaseio.com/heroes/";
   constructor(private http: Http) {}
 
-  nuevoHeroe( heroe:Heroe ) {
-
-    let body = JSON.stringify( heroe );
+  /**
+   * Insertamos un Heroe nuevo pasando el objeto heroe
+   */
+  nuevoHeroe(heroe: Heroe) {
+    let body = JSON.stringify(heroe);
     let headers = new Headers({
-      'Content-Type' : 'application/json'
+      "Content-Type": "application/json"
     });
 
-    return this.http.post( this.heroesURL,body,{headers} )
-      .map( res => {
-        console.log( res.json() );
-        return res.json();
-      });
+    return this.http.post(this.heroesURL, body, { headers }).map(res => {
+      console.log(res.json());
+      return res.json();
+    });
+  }
+  /**
+   * Actualizamos el heroe mandando todo el objeto json
+   * @param heroe
+   * @param key$
+   */
 
+  actualizarHeroe(heroe: Heroe, key$: string) {
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      "Content-Type": "application/json"
+    });
+
+    let url = `${this.heroeURL}/${key$}.json`;
+
+    return this.http.put(this.heroesURL, body, { headers }).map(res => {
+      return res.json();
+    });
+  }
+/**
+ * Obtenemos el Heroe mediante el ID
+ * @param key$
+ */
+  getHeroe(key$: string) {
+    let headers = new Headers({
+      "Content-Type": "application/json"
+    });
+    let url = `${this.heroeURL}/${key$}.json`;
+    return this.http.get(url, { headers }).map(res => {
+      return res.json();
+    });
   }
 }
