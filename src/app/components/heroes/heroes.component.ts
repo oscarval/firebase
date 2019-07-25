6fe54472b6acb1fdd31542062b3a708047a146ee
Alexis <entulinea.alexis@gmail.com>
2019-07-25 18:48:20 +0200

    Get all data of heroes and list on home

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes : Heroe[] = [];
+
+  constructor(private heroesServices:HeroesService) { }
 
   ngOnInit() {
+    this.heroesServices.getHeroes()
+    .subscribe(resp => {
+      this.heroes = resp;
+    });
+
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -7,7 +7,7 @@ import "rxjs";
 export class HeroesService {
   heroesURL: string = "https://heroesapp-6522e.firebaseio.com/heroes.json";
   heroeURL: string = "https://heroesapp-6522e.firebaseio.com/heroes/";
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   /**
    * insert new Heroe send all object json
@@ -41,10 +41,10 @@ export class HeroesService {
       return res.json();
     });
   }
-/**
- * Get the Heroe by ID
- * @param key$
- */
+  /**
+   * Get the Heroe by ID
+   * @param key$
+   */
   getHeroe(key$: string) {
     let headers = new Headers({
       "Content-Type": "application/json"
@@ -53,5 +53,31 @@ export class HeroesService {
     return this.http.get(url, { headers }).map(res => {
       return res.json();
     });
+  }
+  /**
+   * Get all recors of heroes
+   */
+  getHeroes() {
+    let headers = new Headers({
+      "Content-Type": "application/json"
+    });
+    let url = `${this.heroesURL}`;
+    return this.http.get(url, { headers }).map(res => {
+      return this.createArray(res.json());
+    });
+  }
+
+  private createArray(heroesObj: object) {
+    const heroes: Heroe[] = [];
+
+    Object.keys(heroesObj).forEach(key => {
+      const heroe: Heroe = heroesObj[key];
+      heroe.key$ = key;
+      heroes.push(heroe);
+    });
+
+    if (heroesObj === null) { return []; }
+
+    return heroes;
   }
 }
